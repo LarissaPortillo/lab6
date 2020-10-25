a99ecebdc75923981bd5622d8b6be9bbe2dd5ce3
Glitch (hello-webpage) <none>
2020-10-25 02:01:17 +0000

    🚐⏰ Checkpoint ./script.js:21940894/413

--- script.js
+++ script.js
@@ -6,3 +6,17 @@ d3.csv('https://cdn.glitch.com/ee969b39-5890-4207-8b9e-31577b0b6838%2Funemployme
   console.log("data",data);
   
 });
+
+const margin = ({top: 10, right: 10, bottom: 20, left: 40});
+  
+const width = 500-margin.left-margin.right, height = 400-margin.top - margin.bottom;
+
+const svg = d3.select("body")
+    .append("svg")
+    .attr("width", width+margin.left+margin.right)
+    .attr("height", height+margin.top+margin.bottom)
+    .append("g")
+    .attr("transform",`translate(${margin.left}, ${margin.top})`);
+
+
+  
