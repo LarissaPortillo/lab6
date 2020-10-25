ee3b264254f0b8299793b7b78ad26500b84a64d9
Glitch (hello-webpage) <none>
2020-10-25 18:04:51 +0000

    🚜🐛 Checkpoint ./script.js:21940894/4908

--- script.js
+++ script.js
@@ -5,7 +5,31 @@ d3.csv('https://cdn.glitch.com/ee969b39-5890-4207-8b9e-31577b0b6838%2Funemployme
   
   console.log("data",data);
   
-  let total = d3.rollups(data, v=>v.length, d=> d.date)
+  let total = data.row(function(d) { 
+        const total = 0;
+        for (var o in d) {
+          if (o === "date") continue;
+          else total += +d[o];
+        }
+        return {
+          date: d.date, 
+          "Wholesale and Retail Trade": d["Wholesale and Retail Trade"],
+          "Manufacturing":d.Manufacturing,
+          "Leisure and hospitality":d["Leisure and hospitality"],
+          "Business services": d["Business services"],
+          "Construction":d.Construction,
+          "Education and Health":d["Education and Health"],
+          Government:d.Government,
+          Finance:d.Finance,
+          "Self-employed":d["Self-employed"],
+          Other:d.Other,
+          Transportation:d.Transportation,
+          Information:d.Information,
+          Agriculture:d.Agriculture,
+          "Mining and Extraction":d["Mining and Extraction"],
+          total: total
+        };
+      })
 //((+d.v) + (+d.Manufacturing) + (+d.Leisure) + (+d.Business)+ (+d.Construction)+ (+d.Education)+ (+d.Government) + (+d.Finance) + (+d.Self)+ (+d.Other)+ (+d.Transportation)+ (+d.Information)+ (+d.Agriculture)+ (+d.Mining)) ; })
   
   console.log("total",total);
