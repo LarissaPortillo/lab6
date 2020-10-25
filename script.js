897d7f3fcd47419947a07d0c12c6ccf7e6d28910
Glitch (hello-webpage) <none>
2020-10-25 00:02:16 +0000

    ⛩☎️ Checkpoint ./index.html:21940894/844 ./script.js:21940894/1779 ./.glitch-assets:21940894/370

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
-
-// prints "hi" in the browser's dev tools console
-console.log("hi");
+d3.csv('https://cdn.glitch.com/ee969b39-5890-4207-8b9e-31577b0b6838%2Funemployment.csv?v=1603583788416', d3.autoType)
+.then(data=>{
+  
+  data=data;
+  
+  console.log("data",data);
+  
+});
