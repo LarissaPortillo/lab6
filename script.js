9762939198dc0d40ad19321f992a6e95abfded64
Glitch (hello-webpage) <none>
2020-10-28 22:48:21 +0000

    👫📟 Checkpoint ./script.js:21940894/1359

--- script.js
+++ script.js
@@ -93,6 +93,46 @@ d3.csv('https://cdn.glitch.com/ee969b39-5890-4207-8b9e-31577b0b6838%2Funemployme
   
   ), 
   d=> d.date); 
+  
+  const rowTotal=(d=>{
+    let t = d3.rollups(data, v => d3.sum(v, d=>
+    d["Wholesale and Retail Trade"]
+    +d.Manufacturing
+    +d["Leisure and hospitality"]
+    +d["Business services"]
+    +d.Construction
+    +d["Education and Health"]
+    +d.Government
+    +d.Finance
+    +d["Self-employed"]
+    +d.Other
+    +d["Transportation and Utilities"]
+    +d.Information
+    +d.Agriculture
+    +d["Mining and Extraction"]
+  
+  ), 
+  d=> d.date); 
+    return{
+      date: d.date,
+      "Wholesale and Retail Trade":d["Wholesale and Retail Trade"],
+      Manufacturing: d.Manufacturing,
+      "Leisure and hospitality": d["Leisure and hospitality"],
+      "Business services":d["Business services"],
+      Constructuon:d.Construction,
+      "Education and Health": d["Education and Health"],
+      Government:d.Government,
+      Finance: d.Finance,
+      "Self-employed":d["Self-employed"],
+      Other: d.Other,
+      "Transportation and Utilities":d["Transportation and Utilities"],
+      Information: d.Information,
+      Agriculture: d.Agriculture,
+      "Mining and Extraction":d["Mining and Extraction"],
+      total: t[1]
+    };
+  })
+  console.log("row",rowTotal);
     
   console.log("o",origin);
   console.log("total",total)
